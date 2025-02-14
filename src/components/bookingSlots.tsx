0d5b07232ad2f1bb944cab161cbe1701410afa6c
Lang <808xlang@gmail.com>
2025-02-14 00:36:40 -0500

    LangsWorld Gallery Ready

--- src/components/bookingSlots.tsx
+++ src/components/bookingSlots.tsx
@@ -1,21 +1,30 @@
 // components/BookingSlots.js
 import React from 'react';
 
-const BookingSlots = ({ selectedDate, availableSlots, onBook }) => {
+// const BookingSlots = ({ selectedDate, availableSlots, onBook }) => {
+//   return (
+//     <div>
+//       <h2>Available Time Slots for {selectedDate.toDateString()}</h2>
+//       <ul>
+//         {availableSlots.map((slot, index) => (
+//           <li key={index}>
+//             <button onClick={() => onBook(slot)}>
+//               {slot}
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+const BookingSlots = () => {
   return (
     <div>
-      <h2>Available Time Slots for {selectedDate.toDateString()}</h2>
-      <ul>
-        {availableSlots.map((slot, index) => (
-          <li key={index}>
-            <button onClick={() => onBook(slot)}>
-              {slot}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <h2>Under Construction :/</h2>
+      </div>
   );
-};
+}
+
+
 
 export default BookingSlots;
